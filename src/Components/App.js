2645fbb10c8ff3f6e15ad9e2e555c762671f328d
Abhay <abhaykort@gmail.com>
2021-12-29 13:42:53 +0530

    Game Logic done

--- src/Components/App.js
+++ src/Components/App.js
@@ -17,23 +17,97 @@ function App() {
 
   */
   const [pokeObj, setPokeObj] = useState([]);
+  const [clickedPoke, setClickedPoke] = useState([]);
+  const [strikes, setStrikes] = useState(0);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
-    console.log('Mounting Comps')
+    console.log('Mounting Comps');
+    // function setData() {
+    //   for (let i = 0; i < 20; i++) {
+    //     const test = fetch(`https://pokeapi.co/api/v2/pokemon`)
+    //       .then(response => {
+    //         return response.json()
+    //       }).then(reqJSON => {
+    //         let reqObj = {};
+    //         reqObj = (reqJSON.results[i]);
+    //         setPokeObj(pokeObj.push({ index: i + 1, name: reqObj.names, url: reqObj.url, beenClicked: false }))
+    //         console.log(pokeObj);
+    //         return fetch(reqObj.url)
+    //       }).then(resp => {
+    //         return resp.json()
+    //       }).then(
+    //         (r) => {
+    //           console.log(r)
+    //         }
+    //       )
+    //   }
+    // }
     async function setData() {
+      let tempArr = [];
       const req = await fetch(`https://pokeapi.co/api/v2/pokemon`);
-      const reqObj = await req.json()
-      setPokeObj(
-        reqObj.results.map(
-          (items, index) => {
-            console.log(items);
-            return { index: index + 1, name: items.name, url: items.url, beenClicked: false }
-          }
-        )
-      );
+      console.log('Fetching 1')
+      const reqObj = await req.json();
+      // setPokeObj(
+      //   reqObj.results.map(
+      //     (items, index) => {
+      //       return { index: index + 1, name: items.name, url: items.url, beenClicked: false }
+      //     }
+      //   ));
+      for (let i = 0; i < reqObj.results.length; i++) {
+        const newReq = await fetch(reqObj.results[i].url);
+        console.log('Fetching 2')
+        const newReqJSOn = await newReq.json();
+        tempArr.push({ index: i + 1, name: reqObj.results[i].name, url: newReqJSOn.sprites.front_default, beenClicked: false })
+      }
+      setPokeObj(tempArr);
+
     }
     setData();
+
+    // async function fetchPokeData() {
+    //   console.log('Comp Mounting');
+    //   let linkArr = [];
+    //   let copyArr = pokeObj;
+    //   for (let i = 0; i < pokeObj.length; i++) {
+    //     const pokeP = await fetch(`${pokeObj[i].url}`);
+    //     const pokeJSON = await pokeP.json();
+    //     console.log('Requesting Data')
+    //     linkArr.push(await pokeJSON.sprites.front_default);
+    //   }
+    //   for (let j = 0; j < linkArr.length; j++) {
+    //     copyArr[j].url = await linkArr[j];
+    //     console.log(await copyArr[j]);
+    //   }
+    //   console.log(copyArr);
+    //   setPokeObj(copyArr);
+    //   console.log(pokeObj);
+    // }
+    // fetchPokeData();
   }, [])
+
+  function reSeedArr(len = 5) {
+    const arr = [];
+    while (arr.length < len) {
+      const randomNumber = parseInt(Math.random() * 20);
+      if (!arr.includes(randomNumber)) {
+        arr.push(randomNumber)
+      }
+    }
+    return arr;
+  }
+
+  function reArrangeArr() {
+    const seedArr = reSeedArr(pokeObj.length);
+    console.log(seedArr)
+    let localCopyArr = [];
+    for (let i = 0; i < seedArr.length; i++) {
+      let seedNum = seedArr[i];
+      localCopyArr.push(pokeObj[seedNum]);
+    }
+    console.log(localCopyArr);
+    return localCopyArr
+  }
 
 
   function passedOnClick(name) {
@@ -41,8 +115,23 @@ function App() {
     let copyArr = pokeObj;
     for (let i = 0; i < copyArr.length; i++) {
       if (name == copyArr[i].name) {
-        console.log('test');
+        if (copyArr[i].beenClicked == false) {
+          setScore(score + 1);
+          copyArr[i].beenClicked = true;
+        }
+        else {
+          if (strikes < 3) {
+            setStrikes(strikes + 1);
+            alert('Been Clicked already!');
+          }
+          else {
+            alert('Game Over!');
+          }
+        }
+        setPokeObj(copyArr);
+        setPokeObj(reArrangeArr());
       }
+
     }
   }
 
@@ -51,7 +140,6 @@ function App() {
       <div className="App">
         {//Component must be used to handle API Request and assigning them to the Box objs, which contain the sprites, name, beenClicked and event listener
           pokeObj.map((poke) => {
-            console.log(poke);
             return (
               <div>
                 <Pokemon obj={poke} funcSetBeenClicked={passedOnClick} />
@@ -59,6 +147,9 @@ function App() {
             )
           })
         }
+      </div>
+      <div>
+        Scores = {score}
       </div>
     </div>
   );
--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -4,28 +4,12 @@ import './index.css'
 
 export default function Pokemon(props) {
 
-    console.log(props)
-
-    const [pokeURL, setPokeURL] = useState('');
-
-    useEffect(() => {
-
-        async function fetchPokeData() {
-            const pokeP = await fetch(`${props.obj.url}`);
-            const pokeJSON = await pokeP.json();
-            console.log(pokeJSON);
-            setPokeURL(pokeJSON.sprites.front_default)
-        }
-        fetchPokeData();
-    }, [])
-
     return (
         <div className="clikcObj" onClick={() => {
             props.funcSetBeenClicked(props.obj.name);
-            console.log(props);
         }}>
             <div>
-                <img src={`${pokeURL}`} alt={props.obj.name} />
+                <img src={`${props.obj.url}`} alt={props.obj.name} />
             </div>
             <div>
                 {props.obj.index}
